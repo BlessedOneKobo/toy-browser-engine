63311c8bc957a04691c3c77d71b70cac421b2ea6
Abel <59095867+BlessedOneKobo@users.noreply.github.com>
2024-04-29 19:48:31 +0100

    refactor

--- dom.cpp
+++ dom.cpp
@@ -1,73 +1,81 @@
-#include <cstddef>
 #include <iostream>
 #include <map>
+#include <variant>
 #include <vector>
+
+typedef std::map<std::string, std::string> AttrMap;
 
 struct ElementData {
     std::string tag_name;
-    std::map<std::string, std::string> attributes;
+    AttrMap attributes;
 };
+
+typedef std::variant<std::string, ElementData> NodeType;
 
 struct Node {
+    NodeType node_type;
     std::vector<Node> children;
-    std::variant<ElementData, std::string> node_type;
 };
 
-Node createTextNode(const std::string data) {
-    return {.children = {}, .node_type = data};
+Node createTextNode(std::string data) {
+    return {
+        .node_type = data,
+        .children = {},
+    };
 }
 
-Node createElementNode(std::string name,
-                       std::map<std::string, std::string> attributes,
+Node createElementNode(std::string name, AttrMap attrs,
                        std::vector<Node> children) {
-    ElementData data{.tag_name = name, .attributes = attributes};
-    return {.children = children, .node_type = data};
+    ElementData data = {.tag_name = name, .attributes = attrs};
+    return {
+        .node_type = data,
+        .children = children,
+    };
 }
 
-void indent(size_t depth) {
+void printIndent(size_t depth, std::string indent = "  ") {
     std::cout << '\n';
     for (size_t i = 0; i < depth; i++)
-        std::cout << "    ";
+        std::cout << indent;
 }
 
-void printDom(const Node &root, size_t depth = 0) {
+void printTree(const Node &root, size_t depth = 0) {
     if (depth > 0)
-        indent(depth);
+        printIndent(depth);
 
     if (std::holds_alternative<std::string>(root.node_type)) {
         std::cout << "\"" << std::get<std::string>(root.node_type) << "\"";
         return;
     }
 
-    std::cout << "<" << std::get<ElementData>(root.node_type).tag_name;
-    for (const auto &[key, value] :
-         std::get<ElementData>(root.node_type).attributes) {
-        std::cout << ' ' << key << "=\"" << value << "\"";
-    }
-    std::cout << ">";
+    ElementData elem_data = std::get<ElementData>(root.node_type);
+    std::cout << '<' << elem_data.tag_name;
+    for (const auto &[key, value] : elem_data.attributes)
+        std::cout << ' ' << key << "=\"" << value << '"';
+    std::cout << '>';
 
-    for (Node child : root.children) {
-        printDom(child, depth + 1);
-    }
+    for (const Node &child : root.children)
+        printTree(child, depth + 1);
 
-    if (!root.children.empty())
-        indent(depth);
-    std::cout << "</" << std::get<ElementData>(root.node_type).tag_name << ">";
+    if (root.children.size())
+        printIndent(depth);
+    std::cout << "</" << elem_data.tag_name << '>';
 }
 
 int main() {
     Node elem{createElementNode(
         "DIV", {},
         {
-            createElementNode("P", {{"id", "subtitle"}, {"class", "pink"}}, {}),
+            createElementNode("P", {{"ID", "subtitle"}, {"CLASS", "pink"}}, {}),
             createElementNode(
                 "P", {},
                 {createTextNode("hello"),
-                 createElementNode("A", {}, {createTextNode("click me")}),
+                 createElementNode("A", {{"HREF", "https://example.com"}},
+                                   {createTextNode("click me")}),
                  createElementNode(
                      "SPAN", {},
                      {createTextNode("something's in the water")})}),
             createElementNode("P", {}, {}),
         })};
-    printDom(elem);
+    printTree(elem);
 }
